d8b212c603ca8e466582e20b8863656156f805fa
Code Cadet <codecadet@lolcode.local>
2020-08-26 14:45:10 +0100

    stuff

--- mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/persistence/model/Bride.java
+++ mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/persistence/model/Bride.java
@@ -1,4 +1,13 @@
 package org.academiadecodigo.felinux.mammamia.persistence.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * The customer model entity
+ */
+
+@Entity
+@Table(name =
 public class Bride extends AbstractModel {
 }
