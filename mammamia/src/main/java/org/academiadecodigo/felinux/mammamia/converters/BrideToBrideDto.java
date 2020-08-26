efe34a63d32646fd3808e28821d9bcc33079f63f
Code Cadet <codecadet@go.local>
2020-08-26 15:45:25 +0100

    converters add

--- mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/converters/BrideToBrideDto.java
+++ mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/converters/BrideToBrideDto.java
@@ -3,5 +3,23 @@ package org.academiadecodigo.felinux.mammamia.converters;
 import org.academiadecodigo.felinux.mammamia.command.BrideDto;
 import org.academiadecodigo.felinux.mammamia.persistence.model.Bride;
 
-public class BrideToBrideDto extends AbstractConverter<Bride, BrideDto>{
+public class BrideToBrideDto extends AbstractConverter<Bride, BrideDto> {
+
+    @Override
+    public BrideDto convert(Bride bride) {
+
+        BrideDto brideDto = new BrideDto();
+        brideDto.setId(bride.getId());
+        brideDto.setFirstName(bride.getFirstName());
+        brideDto.setLastName(bride.getLastName());
+        brideDto.setNickname(bride.getNickname());
+        brideDto.setBirthDate(bride.getBirthDate());
+        brideDto.setSex(bride.getSex());
+        brideDto.setAboutMe(bride.getAboutMe());
+        brideDto.setFavouriteSong(bride.getFavouriteSong());
+        brideDto.setLocation(bride.getLocation());
+
+        return brideDto;
+    }
+
 }
