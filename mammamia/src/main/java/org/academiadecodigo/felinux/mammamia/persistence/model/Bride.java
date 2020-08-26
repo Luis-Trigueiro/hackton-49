1cc3146ce06a69a44c20b524d21e76e19ff92c47
Code Cadet <codecadet@lolcode.local>
2020-08-26 14:56:13 +0100

    add bride model

--- mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/persistence/model/Bride.java
+++ mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/persistence/model/Bride.java
@@ -1,13 +1,129 @@
 package org.academiadecodigo.felinux.mammamia.persistence.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.academiadecodigo.felinux.mammamia.persistence.model.variables.SexType;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
- * The customer model entity
+ * The bride model entity
  */
-
 @Entity
 @Table(name = "bride")
 public class Bride extends AbstractModel {
+    
+    private String firstName;
+    private String lastName;
+    private String nickname;
+    private Date birthDate;
+    private SexType sex;
+    private String aboutMe;
+    private String favouriteSong;
+    private String Location;
+
+    @OneToMany(
+            // propagate changes on customer entity to account entities
+            cascade = {CascadeType.ALL},
+
+            // make sure to remove accounts if unlinked from customer
+            orphanRemoval = true,
+
+            // user customer foreign key on account table to establish
+            // the many-to-one relationship instead of a join table
+            mappedBy = "customer",
+
+            // fetch accounts from database together with user
+            fetch = FetchType.EAGER
+    )
+    private List<Proposal> proposals = new ArrayList<>();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public SexType getSex() {
+        return sex;
+    }
+
+    public void setSex(SexType sex) {
+        this.sex = sex;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public String getFavouriteSong() {
+        return favouriteSong;
+    }
+
+    public void setFavouriteSong(String favouriteSong) {
+        this.favouriteSong = favouriteSong;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public List<Proposal> getProposals() {
+        return proposals;
+    }
+
+    public void setProposals(List<Proposal> proposals) {
+        this.proposals = proposals;
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    @Override
+    public String toString() {
+
+        // printing recipients with lazy loading
+        // and no session will cause issues
+        return "Bride{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", sex='" + sex + '\'' +
+                ", proposals='" + proposals + '\'' +
+                "} " + super.toString();
+    }
 }
