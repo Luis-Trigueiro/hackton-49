7eb38a9220b4e39836ffcfbbab341f393e2a1fe4
dsrules007 <sousilvaney@gmail.com>
2020-08-26 13:42:46 +0100

    add dependencies

---
+++ mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/MammamiaApplication.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.felinux.mammamia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MammamiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MammamiaApplication.class, args);
+	}
+
+}
---
+++ mammamia/src/test/java/org/academiadecodigo/felinux/mammamia/MammamiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.felinux.mammamia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MammamiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
