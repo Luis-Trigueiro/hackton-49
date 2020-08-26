727ecd85dc6893c55f7c5087bb372818adae1f0b
Code Cadet <codecadet@lolcode.local>
2020-08-26 18:20:49 +0100

    add sql file

--- mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/command/BrideDto.java
+++ mammamia/src/main/java/org/academiadecodigo/felinux/mammamia/command/BrideDto.java
@@ -3,7 +3,6 @@ package org.academiadecodigo.felinux.mammamia.command;
 import org.academiadecodigo.felinux.mammamia.persistence.model.variables.SexType;
 
 import javax.validation.constraints.*;
-import java.util.Date;
 
 public class BrideDto {
 
